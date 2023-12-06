2862aaa5014d5de154156baeeece7ad8431f2984
samuel <shemlouk@gmail.com>
2023-12-06 17:09:49 -0300

    fix: add client disconnection to seed function

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
 const seed = async () => {
+  const prisma = new PrismaClient();
+
   await prisma.room.create({ data: { name: "Global Room" } });
+
+  prisma.$disconnect;
 };
 
 seed();
