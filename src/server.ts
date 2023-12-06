66db69f8d6ff04a5549818c0b641efd300c148f9
samuel <shemlouk@gmail.com>
2023-12-06 19:48:35 -0300

    fix: add host to fastify app

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 import { envs } from "./lib/envs";
 
-app.listen({ port: envs.PORT }).then(() => {
+app.listen({ host: "0.0.0.0", port: envs.PORT }).then(() => {
   console.info("Server started!");
 });
