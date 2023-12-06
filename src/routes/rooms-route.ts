d88eb649c48061376ef4c37aaa3975c443e5e40b
samuel <shemlouk@gmail.com>
2023-12-06 18:24:03 -0300

    feat: add messages endpoint to list last 10 messages from a room

--- src/routes/rooms-route.ts
+++ src/routes/rooms-route.ts
@@ -23,4 +23,39 @@ export async function roomsRoute(app: FastifyInstance) {
 
     reply.send({ room });
   });
+
+  app.get("/:roomId/messages/recent", async (request, reply) => {
+    const { roomId } = request.params as { roomId: string };
+
+    const messages = await prisma.message.findMany({
+      where: { room_id: roomId },
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        room: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+      take: -10,
+    });
+
+    const formattedMessages = messages.map((message) => {
+      return {
+        id: message.id,
+        content: message.content,
+        createdAt: message.created_at,
+        user: message.user,
+        room: message.room,
+      };
+    });
+
+    reply.send({ messages: formattedMessages });
+  });
 }
